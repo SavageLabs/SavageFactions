527fcf53323d2324f815ce88049f57c09de94123
Falistos <contact@falistos.me>
2018-09-05 01:02:29 +0200

    Re-added PlayerVaults integration

--- src/main/java/com/massivecraft/factions/cmd/CmdVault.java
+++ src/main/java/com/massivecraft/factions/cmd/CmdVault.java
@@ -1,16 +1,15 @@
 package com.massivecraft.factions.cmd;
 
-import com.massivecraft.factions.Board;
-import com.massivecraft.factions.FLocation;
-import com.massivecraft.factions.P;
+import com.drtshock.playervaults.PlayerVaults;
+import com.drtshock.playervaults.util.Lang;
+import com.drtshock.playervaults.vaultmanagement.UUIDVaultManager;
+import com.drtshock.playervaults.vaultmanagement.VaultOperations;
+import com.drtshock.playervaults.vaultmanagement.VaultViewInfo;
+import com.massivecraft.factions.Conf;
 import com.massivecraft.factions.struct.Permission;
-import com.massivecraft.factions.zcore.fperms.Access;
-import com.massivecraft.factions.zcore.fperms.PermissableAction;
 import com.massivecraft.factions.zcore.util.TL;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.block.Chest;
-import org.bukkit.inventory.Inventory;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 
 public class CmdVault extends FCommand {
 
@@ -18,7 +17,7 @@ public class CmdVault extends FCommand {
         this.aliases.add("vault");
 
         //this.requiredArgs.add("");
-
+        this.optionalArgs.put("number", "number");
 
         this.permission = Permission.VAULT.node;
         this.disableOnLock = false;
@@ -31,44 +30,49 @@ public class CmdVault extends FCommand {
 
     @Override
     public void perform() {
+        /*
+             /f vault <number>
+         */
 
-        if (!P.p.getConfig().getBoolean("fvault.Enabled")) {
-            fme.sendMessage("This command is disabled!");
-            return;
+        int number = argAsInt(0, 0); // Default to 0 or show on 0
+
+        Player player = me;
+
+        if (PlayerVaults.getInstance().getInVault().containsKey(player.getUniqueId().toString())) {
+            return; // Already in a vault so they must be trying to dupe.
         }
-        Access access = fme.getFaction().getAccess(fme, PermissableAction.VAULT);
-        if (access.equals(Access.DENY)) {
-            fme.msg(TL.GENERIC_NOPERMISSION, "vault");
+
+        int max = myFaction.getMaxVaults();
+        if (number > max) {
+            me.sendMessage(TL.COMMAND_GETVAULT_ALREADYHAVE.format(number, max));
             return;
         }
 
-        if (fme.isInVault()) {
-            me.closeInventory();
-            return;
-        }
-        fme.setInVault(true);
-        Location vaultLocation = fme.getFaction().getVault();
-        if (vaultLocation == null) {
-            fme.msg(TL.COMMAND_VAULT_INVALID);
-            return;
-        }
-        FLocation vaultFLocation = new FLocation(vaultLocation);
-        if (Board.getInstance().getFactionAt(vaultFLocation) != fme.getFaction()) {
-            fme.getFaction().setVault(null);
-            fme.msg(TL.COMMAND_VAULT_INVALID);
-            return;
-        }
-        if (vaultLocation.getBlock().getType() != Material.CHEST) {
-            fme.getFaction().setVault(null);
-            fme.msg(TL.COMMAND_VAULT_INVALID);
-            return;
-        }
-        Chest chest = (Chest) vaultLocation.getBlock().getState();
-        Inventory chestInv = chest.getBlockInventory();
-        fme.msg(TL.COMMAND_VAULT_OPENING);
-        me.openInventory(chestInv);
+        // Something like faction-id
+        String vaultName = String.format(Conf.vaultPrefix, myFaction.getId());
 
+        if (number < 1) {
+            // Message about which vaults that Faction has.
+            // List the target
+            YamlConfiguration file = UUIDVaultManager.getInstance().getPlayerVaultFile(vaultName);
+            if (file == null) {
+                sender.sendMessage(Lang.TITLE.toString() + Lang.VAULT_DOES_NOT_EXIST.toString());
+            } else {
+                StringBuilder sb = new StringBuilder();
+                for (String key : file.getKeys(false)) {
+                    sb.append(key.replace("vault", "")).append(" ");
+                }
 
+                sender.sendMessage(Lang.TITLE.toString() + Lang.EXISTING_VAULTS.toString().replaceAll("%p", fme.getTag()).replaceAll("%v", sb.toString().trim()));
+            }
+            return;
+        } // end listing vaults.
+
+        // Attempt to open vault.
+        if (VaultOperations.openOtherVault(player, null, String.valueOf(number))) {
+            // Success
+            PlayerVaults.getInstance().getInVault().put(player.getUniqueId().toString(), new VaultViewInfo(vaultName, number));
+        }
     }
 
     @Override
