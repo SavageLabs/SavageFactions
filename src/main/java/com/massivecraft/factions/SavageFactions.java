dc1398ce28357e7e78ab62bfba5c283fe857e2c0
ProSavage <naman0311@gmail.com>
2019-07-02 09:27:40 -0500

    Warzone Spam Fix

--- src/main/java/com/massivecraft/factions/SavageFactions.java
+++ src/main/java/com/massivecraft/factions/SavageFactions.java
@@ -560,7 +560,6 @@ public class SavageFactions extends MPlugin {
         List<MCommand<?>> commandsList = cmdBase.subCommands;
 
         if (Board.getInstance().getFactionAt(new FLocation(fPlayer.getPlayer().getLocation())) == Factions.getInstance().getWarZone()) {
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cYou cannot use autocomplete in warzone."));
             return new ArrayList<>();
         }
 
