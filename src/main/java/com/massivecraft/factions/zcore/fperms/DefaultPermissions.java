f99c2f3011577d41c20cb020e203f40983ad773a
Savag3life <unknowngaming878@gmail.com>
2019-07-18 20:18:13 -0300

    Default Perm for /f home fixed

--- src/main/java/com/massivecraft/factions/zcore/fperms/DefaultPermissions.java
+++ src/main/java/com/massivecraft/factions/zcore/fperms/DefaultPermissions.java
@@ -128,6 +128,7 @@ public class DefaultPermissions {
         else if (name == "door") return this.door;
         else if (name == "button") return this.button;
         else if (name == "lever") return this.lever;
+        else if (name == "home") return this.home;
         else if (name == "container") return this.container;
         else if (name == "invite") return this.invite;
         else if (name == "kick") return this.kick;
@@ -135,7 +136,6 @@ public class DefaultPermissions {
         else if (name == "sethome") return this.sethome;
         else if (name == "territory") return this.territory;
         else if (name == "access") return this.access;
-        else if (name == "home") return this.disband;
         else if (name == "disband") return this.disband;
         else if (name == "promote") return this.promote;
         else if (name == "setwarp") return this.setwarp;
