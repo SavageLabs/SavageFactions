f06a530af109a175cf219bb36c466d3c66f71828
Savag3life <unknowngaming878@gmail.com>
2019-07-18 21:38:50 -0300

    F-TNT Take giving incorrect amount fix

--- src/main/java/com/massivecraft/factions/cmd/CmdTnt.java
+++ src/main/java/com/massivecraft/factions/cmd/CmdTnt.java
@@ -102,8 +102,8 @@ public class CmdTnt extends FCommand {
                     fme.msg(TL.COMMAND_TNT_WIDTHDRAW_NOTENOUGH.toString());
                     return;
                 }
-                int fullStacks = amount / 64;
-                int remainderAmt = amount % 64;
+                int fullStacks = Math.round(amount / 64);
+                int remainderAmt = amount - (fullStacks * 64);
                 if ((remainderAmt == 0 && !hasAvaliableSlot(me, fullStacks))) {
                     fme.msg(TL.COMMAND_TNT_WIDTHDRAW_NOTENOUGH_SPACE);
                     return;
@@ -112,14 +112,10 @@ public class CmdTnt extends FCommand {
                     fme.msg(TL.COMMAND_TNT_WIDTHDRAW_NOTENOUGH_SPACE);
                     return;
                 }
-                ItemStack tnt64 = new ItemStack(Material.TNT, 64);
-                for (int i = 0; i <= fullStacks - 1; i++) {
-                    me.getInventory().addItem(tnt64);
-                }
-                if (remainderAmt != 0) {
-                    ItemStack tnt = new ItemStack(Material.TNT, remainderAmt);
-                    me.getInventory().addItem(tnt);
-                }
+
+                for (int i = 0; i <= fullStacks - 1; i++) me.getPlayer().getInventory().addItem(new ItemStack(Material.TNT, 64));
+                if (remainderAmt != 0) me.getPlayer().getInventory().addItem(new ItemStack(Material.TNT, remainderAmt));
+
                 fme.getFaction().takeTnt(amount);
                 me.updateInventory();
                 fme.msg(TL.COMMAND_TNT_WIDTHDRAW_SUCCESS);
