621f3417b707e625b49dc8e01f3a53588aaea646
Ryder Belserion <Kolt0517@gmail.com>
2019-07-29 02:00:58 -0400

    added messages enum

---
+++ src/main/java/com/massivecraft/factions/util/fm/enums/Messages.java
@@ -0,0 +1,164 @@
+package com.massivecraft.factions.util.fm.enums;
+
+import com.massivecraft.factions.util.fm.FileManager.Files;
+import com.massivecraft.factions.zcore.util.TextUtil;
+import org.bukkit.configuration.file.FileConfiguration;
+import java.util.HashMap;
+import java.util.List;
+
+public enum Messages {
+    CONFIG_RELOAD("config-reload", "&cYou have reloaded the plugin, It took %ms% to complete this task.");
+    
+    private String path;
+    private String defaultMessage;
+    private List<String> defaultListMessage;
+
+    private Messages(String path, String defaultMessage) {
+        this.path = path;
+        this.defaultMessage = defaultMessage;
+    }
+
+    private Messages(String path, List<String> defaultListMessage) {
+        this.path = path;
+        this.defaultListMessage = defaultListMessage;
+    }
+
+    public static String convertList(List<String> list) {
+        String message = "";
+        for (String m : list) {
+            message += TextUtil.parseColor(m) + "\n";
+        }
+        return message;
+    }
+
+    public static String convertList(List<String> list, HashMap<String, String> placeholders) {
+        String message = "";
+        for (String m : list) {
+            message += TextUtil.parseColor(m) + "\n";
+        }
+        for (String ph : placeholders.keySet()) {
+            message = TextUtil.parseColor(message.replaceAll(ph, placeholders.get(ph)));
+        }
+        return message;
+    }
+
+    public static void addMissingMessages() {
+        FileConfiguration messages = Files.LANG.getFile();
+        boolean saveFile = false;
+        for (Messages message : values()) {
+            if (!messages.contains("Messages." + message.getPath())) {
+                saveFile = true;
+                if (message.getDefaultMessage() != null) {
+                    messages.set("Messages." + message.getPath(), message.getDefaultMessage());
+                } else {
+                    messages.set("Messages." + message.getPath(), message.getDefaultListMessage());
+                }
+            }
+        }
+        if (saveFile) {
+            Files.LANG.saveFile();
+        }
+    }
+
+    public String getMessage() {
+        if (isList()) {
+            if (exists()) {
+                return TextUtil.parseColor(convertList(Files.LANG.getFile().getStringList("Messages." + path)));
+            } else {
+                return TextUtil.parseColor(convertList(getDefaultListMessage()));
+            }
+        } else {
+            if (exists()) {
+                return TextUtil.getPrefix(Files.LANG.getFile().getString("Messages." + path));
+            } else {
+                return TextUtil.getPrefix(getDefaultMessage());
+            }
+        }
+    }
+
+    public String getMessage(HashMap<String, String> placeholders) {
+        String message;
+        if (isList()) {
+            if (exists()) {
+                message = TextUtil.parseColor(convertList(Files.LANG.getFile().getStringList("Messages." + path), placeholders));
+            } else {
+                message = TextUtil.parseColor(convertList(getDefaultListMessage(), placeholders));
+            }
+        } else {
+            if (exists()) {
+                message = TextUtil.getPrefix(Files.LANG.getFile().getString("Messages." + path));
+            } else {
+                message = TextUtil.getPrefix(getDefaultMessage());
+            }
+            for (String ph : placeholders.keySet()) {
+                if (message.contains(ph)) {
+                    message = message.replaceAll(ph, placeholders.get(ph));
+                }
+            }
+        }
+        return message;
+    }
+
+    public String getMessageNoPrefix() {
+        if (isList()) {
+            if (exists()) {
+                return TextUtil.parseColor(convertList(Files.LANG.getFile().getStringList("Messages." + path)));
+            } else {
+                return TextUtil.parseColor(convertList(getDefaultListMessage()));
+            }
+        } else {
+            if (exists()) {
+                return TextUtil.parseColor(Files.LANG.getFile().getString("Messages." + path));
+            } else {
+                return TextUtil.parseColor(getDefaultMessage());
+            }
+        }
+    }
+
+    public String getMessageNoPrefix(HashMap<String, String> placeholders) {
+        String message;
+        if (isList()) {
+            if (exists()) {
+                message = TextUtil.parseColor(convertList(Files.LANG.getFile().getStringList("Messages." + path), placeholders));
+            } else {
+                message = TextUtil.parseColor(convertList(getDefaultListMessage(), placeholders));
+            }
+        } else {
+            if (exists()) {
+                message = TextUtil.parseColor(Files.LANG.getFile().getString("Messages." + path));
+            } else {
+                message = TextUtil.parseColor(getDefaultMessage());
+            }
+            for (String ph : placeholders.keySet()) {
+                if (message.contains(ph)) {
+                    message = message.replaceAll(ph, placeholders.get(ph));
+                }
+            }
+        }
+        return message;
+    }
+
+    private boolean exists() {
+        return Files.LANG.getFile().contains("Messages." + path);
+    }
+
+    private boolean isList() {
+        if (Files.LANG.getFile().contains("Messages." + path)) {
+            return !Files.LANG.getFile().getStringList("Messages." + path).isEmpty();
+        } else {
+            return defaultMessage == null;
+        }
+    }
+
+    private String getPath() {
+        return path;
+    }
+
+    private String getDefaultMessage() {
+        return defaultMessage;
+    }
+
+    private List<String> getDefaultListMessage() {
+        return defaultListMessage;
+    }
+}
