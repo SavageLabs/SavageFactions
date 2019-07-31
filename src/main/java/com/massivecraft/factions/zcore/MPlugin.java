803419f05eb6b27eb628ffb5a2c928825ed51d6a
Ryder Belserion <Kolt0517@gmail.com>
2019-07-30 20:22:34 -0400

    removed old langs generator

--- src/main/java/com/massivecraft/factions/zcore/MPlugin.java
+++ src/main/java/com/massivecraft/factions/zcore/MPlugin.java
@@ -13,7 +13,6 @@ import com.massivecraft.factions.zcore.util.Persist;
 import com.massivecraft.factions.zcore.util.TextUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.*;
@@ -99,7 +98,7 @@ public abstract class MPlugin extends JavaPlugin {
             saveTask = Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, new SaveTask(this), saveTicks, saveTicks);
         }
 
-        loadLang();
+       // loadLang();
 
         loadSuccessful = true;
         return true;
@@ -108,231 +107,165 @@ public abstract class MPlugin extends JavaPlugin {
     public void postEnable() {
         log("=== ENABLE DONE (Took " + (System.currentTimeMillis() - timeEnableStart) + "ms) ===");
     }
+        public void onDisable () {
+            if (saveTask != null) {
+                this.getServer().getScheduler().cancelTask(saveTask);
+                saveTask = null;
+            }
+            // only save data if plugin actually loaded successfully
+            if (loadSuccessful) {
+                Factions.getInstance().forceSave();
+                FPlayers.getInstance().forceSave();
+                Board.getInstance().forceSave();
+            }
+            log("Disabled");
+        }
 
-    public void loadLang() {
-        File lang = new File(getDataFolder(), "lang.yml");
-        OutputStream out = null;
-        InputStream defLangStream = this.getResource("lang.yml");
-        if (!lang.exists()) {
-            try {
-                getDataFolder().mkdir();
-                lang.createNewFile();
-                if (defLangStream != null) {
-                    out = new FileOutputStream(lang);
-                    int read;
-                    byte[] bytes = new byte[1024];
+        // -------------------------------------------- //
+        // Some inits...
+        // You are supposed to override these in the plugin if you aren't satisfied with the defaults
+        // The goal is that you always will be satisfied though.
+        // -------------------------------------------- //
 
-                    while ((read = defLangStream.read(bytes)) != -1) {
-                        out.write(bytes, 0, read);
-                    }
-                    YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(new BufferedReader(new InputStreamReader(defLangStream)));
-                    TL.setFile(defConfig);
-                }
-            } catch (IOException e) {
-                e.printStackTrace(); // So they notice
-                getLogger().severe("[Factions] Couldn't create language file.");
-                getLogger().severe("[Factions] This is a fatal error. Now disabling");
-                this.setEnabled(false); // Without it loaded, we can't send them messages
-            } finally {
-                if (defLangStream != null) {
-                    try {
-                        defLangStream.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-                if (out != null) {
-                    try {
-                        out.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+        public void suicide () {
+            log("Now I suicide!");
+            this.getServer().getPluginManager().disablePlugin(this);
+        }
 
-                }
+        // -------------------------------------------- //
+        // LANG AND TAGS
+        // -------------------------------------------- //
+
+        public GsonBuilder getGsonBuilder () {
+            return new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().serializeNulls().excludeFieldsWithModifiers(Modifier.TRANSIENT, Modifier.VOLATILE);
+        }
+
+        public void addRawTags () {
+            this.rawTags.put("l", "<green>"); // logo
+            this.rawTags.put("a", "<gold>"); // art
+            this.rawTags.put("n", "<silver>"); // notice
+            this.rawTags.put("i", "<yellow>"); // info
+            this.rawTags.put("g", "<lime>"); // good
+            this.rawTags.put("b", "<rose>"); // bad
+            this.rawTags.put("h", "<pink>"); // highligh
+            this.rawTags.put("c", "<aqua>"); // command
+            this.rawTags.put("plugin", "<teal>"); // parameter
+        }
+
+        public void initTXT () {
+            this.addRawTags();
+
+            Type type = new TypeToken<Map<String, String>>() {
+            }.getType();
+
+            Map<String, String> tagsFromFile = this.persist.load(type, "tags");
+            if (tagsFromFile != null) {
+                this.rawTags.putAll(tagsFromFile);
+            }
+            this.persist.save(this.rawTags, "tags");
+
+            for (Entry<String, String> rawTag : this.rawTags.entrySet()) {
+                this.txt.tags.put(rawTag.getKey(), TextUtil.parseColor(rawTag.getValue()));
             }
         }
 
-        YamlConfiguration conf = YamlConfiguration.loadConfiguration(lang);
-        for (TL item : TL.values()) {
-            if (conf.getString(item.getPath()) == null) {
-                conf.set(item.getPath(), item.getDefault());
-            }
+        // -------------------------------------------- //
+        // COMMAND HANDLING
+        // -------------------------------------------- //
+
+        // can be overridden by SavageFactions method, to provide option
+        public boolean logPlayerCommands () {
+            return true;
         }
 
-        // Remove this here because I'm sick of dealing with bug reports due to bad decisions on my part.
-        if (conf.getString(TL.COMMAND_SHOW_POWER.getPath(), "").contains("%5$s")) {
-            conf.set(TL.COMMAND_SHOW_POWER.getPath(), TL.COMMAND_SHOW_POWER.getDefault());
-            log(Level.INFO, "Removed errant format specifier from f show power.");
+        public boolean handleCommand (CommandSender sender, String commandString,boolean testOnly){
+            return handleCommand(sender, commandString, testOnly, false);
         }
 
-        TL.setFile(conf);
-        try {
-            conf.save(lang);
-        } catch (IOException e) {
-            getLogger().log(Level.WARNING, "Factions: Failed to save lang.yml.");
-            getLogger().log(Level.WARNING, "Factions: Report this stack trace to prosavage.");
-            e.printStackTrace();
-        }
-    }
-
-    public void onDisable() {
-        if (saveTask != null) {
-            this.getServer().getScheduler().cancelTask(saveTask);
-            saveTask = null;
-        }
-        // only save data if plugin actually loaded successfully
-        if (loadSuccessful) {
-            Factions.getInstance().forceSave();
-            FPlayers.getInstance().forceSave();
-            Board.getInstance().forceSave();
-        }
-        log("Disabled");
-    }
-
-    // -------------------------------------------- //
-    // Some inits...
-    // You are supposed to override these in the plugin if you aren't satisfied with the defaults
-    // The goal is that you always will be satisfied though.
-    // -------------------------------------------- //
-
-    public void suicide() {
-        log("Now I suicide!");
-        this.getServer().getPluginManager().disablePlugin(this);
-    }
-
-    // -------------------------------------------- //
-    // LANG AND TAGS
-    // -------------------------------------------- //
-
-    public GsonBuilder getGsonBuilder() {
-        return new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().serializeNulls().excludeFieldsWithModifiers(Modifier.TRANSIENT, Modifier.VOLATILE);
-    }
-
-    public void addRawTags() {
-        this.rawTags.put("l", "<green>"); // logo
-        this.rawTags.put("a", "<gold>"); // art
-        this.rawTags.put("n", "<silver>"); // notice
-        this.rawTags.put("i", "<yellow>"); // info
-        this.rawTags.put("g", "<lime>"); // good
-        this.rawTags.put("b", "<rose>"); // bad
-        this.rawTags.put("h", "<pink>"); // highligh
-        this.rawTags.put("c", "<aqua>"); // command
-        this.rawTags.put("plugin", "<teal>"); // parameter
-    }
-
-    public void initTXT() {
-        this.addRawTags();
-
-        Type type = new TypeToken<Map<String, String>>() {
-        }.getType();
-
-        Map<String, String> tagsFromFile = this.persist.load(type, "tags");
-        if (tagsFromFile != null) {
-            this.rawTags.putAll(tagsFromFile);
-        }
-        this.persist.save(this.rawTags, "tags");
-
-        for (Entry<String, String> rawTag : this.rawTags.entrySet()) {
-            this.txt.tags.put(rawTag.getKey(), TextUtil.parseColor(rawTag.getValue()));
-        }
-    }
-
-    // -------------------------------------------- //
-    // COMMAND HANDLING
-    // -------------------------------------------- //
-
-    // can be overridden by SavageFactions method, to provide option
-    public boolean logPlayerCommands() {
-        return true;
-    }
-
-    public boolean handleCommand(CommandSender sender, String commandString, boolean testOnly) {
-        return handleCommand(sender, commandString, testOnly, false);
-    }
-
-    public boolean handleCommand(final CommandSender sender, String commandString, boolean testOnly, boolean async) {
-        boolean noSlash = true;
-        if (commandString.startsWith("/")) {
-            noSlash = false;
-            commandString = commandString.substring(1);
-        }
-
-        for (final MCommand<?> command : this.getBaseCommands()) {
-            if (noSlash && !command.allowNoSlashAccess) {
-                continue;
+        public boolean handleCommand ( final CommandSender sender, String commandString,boolean testOnly, boolean async)
+        {
+            boolean noSlash = true;
+            if (commandString.startsWith("/")) {
+                noSlash = false;
+                commandString = commandString.substring(1);
             }
 
-            for (String alias : command.aliases) {
-                // disallow double-space after alias, so specific commands can be prevented (preventing "f home" won't prevent "f  home")
-                if (commandString.startsWith(alias + "  ")) {
-                    return false;
+            for (final MCommand<?> command : this.getBaseCommands()) {
+                if (noSlash && !command.allowNoSlashAccess) {
+                    continue;
                 }
 
-                if (commandString.startsWith(alias + " ") || commandString.equals(alias)) {
-                    final List<String> args = new ArrayList<>(Arrays.asList(commandString.split("\\s+")));
-                    args.remove(0);
+                for (String alias : command.aliases) {
+                    // disallow double-space after alias, so specific commands can be prevented (preventing "f home" won't prevent "f  home")
+                    if (commandString.startsWith(alias + "  ")) {
+                        return false;
+                    }
 
-                    if (testOnly) {
+                    if (commandString.startsWith(alias + " ") || commandString.equals(alias)) {
+                        final List<String> args = new ArrayList<>(Arrays.asList(commandString.split("\\s+")));
+                        args.remove(0);
+
+                        if (testOnly) {
+                            return true;
+                        }
+
+                        if (async) {
+                            Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+                                @Override
+                                public void run() {
+                                    command.execute(sender, args);
+                                }
+                            });
+                        } else {
+                            command.execute(sender, args);
+                        }
+
                         return true;
                     }
-
-                    if (async) {
-                        Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
-                            @Override
-                            public void run() {
-                                command.execute(sender, args);
-                            }
-                        });
-                    } else {
-                        command.execute(sender, args);
-                    }
-
-                    return true;
                 }
             }
+            return false;
         }
-        return false;
+
+        public boolean handleCommand (CommandSender sender, String commandString){
+            return this.handleCommand(sender, commandString, false);
+        }
+
+        // -------------------------------------------- //
+        // HOOKS
+        // -------------------------------------------- //
+        public void preAutoSave () {
+
+        }
+
+        public void postAutoSave () {
+
+        }
+
+        public Map<UUID, Integer> getStuckMap () {
+            return this.stuckMap;
+        }
+
+        public Map<UUID, Long> getTimers () {
+            return this.timers;
+        }
+
+        // -------------------------------------------- //
+        // LOGGING
+        // -------------------------------------------- //
+        public void log (Object msg){
+            log(Level.INFO, msg);
+        }
+
+        public void log (String str, Object...args){
+            log(Level.INFO, this.txt.parse(str, args));
+        }
+
+        public void log (Level level, String str, Object...args){
+            log(level, this.txt.parse(str, args));
+        }
+
+        public void log (Level level, Object msg){
+            getLogger().log(level, "[" + this.getDescription().getFullName() + "] " + msg);
+        }
     }
-
-    public boolean handleCommand(CommandSender sender, String commandString) {
-        return this.handleCommand(sender, commandString, false);
-    }
-
-    // -------------------------------------------- //
-    // HOOKS
-    // -------------------------------------------- //
-    public void preAutoSave() {
-
-    }
-
-    public void postAutoSave() {
-
-    }
-
-    public Map<UUID, Integer> getStuckMap() {
-        return this.stuckMap;
-    }
-
-    public Map<UUID, Long> getTimers() {
-        return this.timers;
-    }
-
-    // -------------------------------------------- //
-    // LOGGING
-    // -------------------------------------------- //
-    public void log(Object msg) {
-        log(Level.INFO, msg);
-    }
-
-    public void log(String str, Object... args) {
-        log(Level.INFO, this.txt.parse(str, args));
-    }
-
-    public void log(Level level, String str, Object... args) {
-        log(level, this.txt.parse(str, args));
-    }
-
-    public void log(Level level, Object msg) {
-        getLogger().log(level, "[" + this.getDescription().getFullName() + "] " + msg);
-    }
-}
