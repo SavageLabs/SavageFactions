89fa73e9efa4b4ec33c6a121bb9c37e49f5d56c0
prosavage <naman0311@gmail.com>
2019-09-14 15:17:48 -0500

    Remove redundant NPE check.

--- src/main/java/com/massivecraft/factions/integration/Essentials.java
+++ src/main/java/com/massivecraft/factions/integration/Essentials.java
@@ -45,7 +45,6 @@ public class Essentials {
 			User user = essentials.getUser(player);
 			vanish = user.isVanished();
 		}
-		if (player.getMetadata("vanished") == null) return vanish;
 		for (MetadataValue meta : player.getMetadata("vanished")) {
 			if (meta == null) continue;
  			if (meta.asBoolean()) {
